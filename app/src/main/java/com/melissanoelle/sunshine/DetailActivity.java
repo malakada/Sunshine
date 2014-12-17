20d57a3d4bce0a535ef49c93139846d8cb1db8a1
Melissa Noelle <melissamatara@gmail.com>
2014-12-17 01:40:05 -0500

    Fix so that app doesn't crash now.

--- app/src/main/java/com/melissanoelle/sunshine/DetailActivity.java
+++ app/src/main/java/com/melissanoelle/sunshine/DetailActivity.java
@@ -19,7 +19,7 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new DetailFragment())
